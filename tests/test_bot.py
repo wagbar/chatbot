749c0a0856ddf8e45454e51d1d7895a5dd589374
wagbar <wagkorg@gmail.com>
2025-04-21 19:42:26 -0300

    Update test_bot.py
    alteração resposta de teste

--- tests/test_bot.py
+++ tests/test_bot.py
@@ -4,4 +4,4 @@ def test_resposta_oi():
     assert responder("oi") == "Olá! Como posso ajudar?"
 
 def test_resposta_desconhecida():
-    assert responder("qual é a sua idade?") == "Desculpe, não entendi o que você quis dizer."
+    assert responder("qual é a sua idade?") == "Eu não tenho uma idade definida porque sou um robô"
